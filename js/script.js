b73d236730e5800fd38496c36fc049cd71461060
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-06-22 20:47:41 -0400

    updated README

--- js/script.js
+++ js/script.js
@@ -8,12 +8,33 @@
  * Check service worker.
  */
  if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS20-PWA-Template/sw.js", {
-    scope: "/ICS20-PWA-Template/",
+  navigator.serviceWorker.register("/ICS2O-UNIT-6-04-HTML-EXTRA/sw.js", {
+    scope: "/ICS2O-UNIT-6-04-HTML-EXTRA/",
   })
 }
 
 /**
  * Input.
  */
+
+ window.onload = function() {
+  // this calculates volume of a pyramid
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const sideA = parseInt(params.get('a'))
+  const sideB = parseInt(params.get('b'))
+  const sideC = parseInt(params.get('C'))
+  const sideD = parseInt(params.get('D'))
+
+
+  // process
+  const area = ((sideA + sideB) / 2) * height
+  const dimensions = "<ul>\n<li>a side = " + sideA + "</li>\n<li>b side = " + sideB + "</li>\n<li>c side = " + sideC + "</li>\n<li>d side = " + sideD + "</li>\n</ul>"
+
+  // output
+  document.getElementById('dimensions').innerHTML = dimensions
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' mm²'
+}
 
